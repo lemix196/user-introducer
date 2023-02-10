78c16b2bb9011f46528e452a45fc23ed7b57444b
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-02-10 22:26:05 +0100

    add form states handlers and whole form logic

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -1,23 +1,53 @@
+import React, { useState } from "react";
+
 import styles from "./UserForm.module.css";
-import FormField from "./FormField";
+// import FormField from "./FormField";
 
 const UserForm = (props) => {
-  const FORM_FIELDS = [
-    { label: "Username", inputType: "text" },
-    { label: "Age (years)", inputType: "text" },
-  ];
+  //   const FORM_FIELDS = [
+  //     { label: "Username", inputType: "text" },
+  //     { label: "Age (years)", inputType: "text" },
+  //   ];
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(enteredAge);
+  };
 
   return (
-    <form>
-      <div className={styles['form-field']}>
+    <form onSubmit={submitFormHandler}>
+      <div className={styles["form-field"]}>
         <label className={styles.label}>Username</label>
-        <input className={styles['text-input']} type="text"></input>
+        <input
+          className={styles["text-input"]}
+          value={enteredName}
+          onChange={nameChangeHandler}
+          type="text"
+        ></input>
       </div>
-      <div className={styles['form-field']}>
+      <div className={styles["form-field"]}>
         <label className={styles.label}>Age (years)</label>
-        <input className={styles['text-input']} type="text"></input>
+        <input
+          className={styles["text-input"]}
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          type="text"
+        ></input>
       </div>
-      <button className={styles.button} type="submit">Add User</button>
+      <button type="submit" className={styles.button}>
+        Add User
+      </button>
     </form>
   );
 };
